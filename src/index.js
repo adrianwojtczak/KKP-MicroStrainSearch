e7764b9f401065eb3be6cb2f7e3ba977e939c47d
Adrian Wojtczak <117528537+adrianwojtczak@users.noreply.github.com>
2023-09-27 20:32:46 +0200

    Add modal

--- src/index.js
+++ src/index.js
@@ -1,62 +1,95 @@
-import * as strainsData from "./data/strains.JSON";
+import * as strainsData from './data/strains.JSON';
 
 const strainsList = strainsData.strains;
 
 console.log(strainsList);
 
-const searchInput = document.getElementById("searchInput");
-const searchBtn = document.getElementById("searchBtn");
-const strainsContainer = document.getElementById("strainsContainer");
+const searchInput = document.getElementById('searchInput');
+const searchBtn = document.getElementById('searchBtn');
+const strainsContainer = document.getElementById('strainsContainer');
 
-searchBtn.addEventListener("click", (ev) => {
-  ev.preventDefault();
-  const searchValue = searchInput.value.trim().toLowerCase();
-  console.log(searchValue);
+const modal = document.getElementById('modal');
+const modalContent = document.getElementById('modalContent');
+const modalStrainInfo = document.getElementById('modalStrainInfo');
+const modalCloseBtn = document.querySelector('.close');
 
-  const filteredStrains = searchStrains(searchValue);
-  displayStrains(filteredStrains);
+searchBtn.addEventListener('click', ev => {
+	ev.preventDefault();
+	const searchValue = searchInput.value.trim().toLowerCase();
+	console.log(searchValue);
+
+	const filteredStrains = searchStrains(searchValue);
+	displayStrains(filteredStrains);
 });
 
 function searchStrains(searchTerm) {
-  const filteredStrains = strainsList.filter((strain) => {
-    const strainName = strain.strain.trim().toLowerCase();
-    const strainNo = strain.no.trim().toLowerCase();
-    return strainName.includes(searchTerm) || strainNo.includes(searchTerm);
-  });
-  return filteredStrains;
+	const filteredStrains = strainsList.filter(strain => {
+		const strainName = strain.strain.trim().toLowerCase();
+		const strainNo = strain.no.trim().toLowerCase();
+		return strainName.includes(searchTerm) || strainNo.includes(searchTerm);
+	});
+	return filteredStrains;
 }
 
-console.log(searchStrains("bacillus"));
+console.log(searchStrains('bacillus'));
 
 function displayStrains(strains) {
-  strainsContainer.innerHTML = "";
+	strainsContainer.innerHTML = '';
 
-  strains.forEach((strain) => {
-    const strainEl = document.createElement("div");
+	strains.forEach(strain => {
+		const strainEl = document.createElement('div');
+		strainEl.classList.add('strain');
 
-    for (const key in strain) {
-      const value = strain[key];
+		const nameAndNumber = document.createElement('p');
+		nameAndNumber.textContent = `${strain.strain} ${strain.no}`;
+		nameAndNumber.classList.add('strain-info');
+		strainEl.appendChild(nameAndNumber);
 
-      if (typeof value === "object") {
-        const nestedDiv = document.createElement("div");
-        strainEl.style.backgroundColor = "#f2f2f2";
-        strainEl.style.marginBottom = "10px";
+		strainEl.addEventListener('click', () => {
+			openModal(strain);
+		});
 
-        for (const nestedKey in value) {
-          const nestedValue = value[nestedKey];
-          const nestedKeyValueEl = document.createElement("p");
-          nestedKeyValueEl.textContent = `${nestedKey}: ${nestedValue}`;
-          nestedDiv.appendChild(nestedKeyValueEl);
-        }
+		strainsContainer.appendChild(strainEl);
+	});
+}
 
-        strainEl.appendChild(nestedDiv);
-      } else {
-        const keyValueEl = document.createElement("p");
-        keyValueEl.textContent = `${key}: ${value}`;
-        strainEl.appendChild(keyValueEl);
-      }
-    }
+function openModal(strain) {
+	modal.style.display = 'block';
+	modalStrainInfo.innerHTML = '';
 
-    strainsContainer.appendChild(strainEl);
-  });
+	const modalTitle = document.createElement('h2');
+	modalTitle.textContent = `${strain.strain} ${strain.no}`;
+	modalStrainInfo.appendChild(modalTitle);
+
+	const modalInfoList = document.createElement('ul');
+
+	const isolationInfo = strain.isolation;
+	if (isolationInfo) {
+		const isolationItem = document.createElement('li');
+		isolationItem.textContent = `Isolation Year: ${isolationInfo.year}, Place: ${isolationInfo.place}`;
+		modalInfoList.appendChild(isolationItem);
+	}
+
+	const growthConditionsInfo = strain.growthConditions;
+	if (growthConditionsInfo) {
+		const growthConditionsItem = document.createElement('li');
+		growthConditionsItem.textContent = `Growth Temperature: ${growthConditionsInfo.temperature}, Conditions: ${growthConditionsInfo.conditions}`;
+		modalInfoList.appendChild(growthConditionsItem);
+	}
+
+	modalStrainInfo.appendChild(modalInfoList);
+}
+
+modalCloseBtn.addEventListener('click', () => {
+	closeModal();
+});
+
+window.addEventListener('click', event => {
+	if (event.target === modal) {
+		closeModal();
+	}
+});
+
+function closeModal() {
+	modal.style.display = 'none';
 }
