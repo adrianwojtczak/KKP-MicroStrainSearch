998ab7645f41367cb07192cb5d25ababf72230f7
Adrian Wojtczak <117528537+adrianwojtczak@users.noreply.github.com>
2024-01-14 18:19:44 +0100

    Refactor search

--- src/search.js
+++ src/search.js
@@ -1,6 +1,6 @@
 import * as strainsData from './data/strains.JSON';
 
-const strainsList = strainsData.strains;
+const strainsList = strainsData.IBPRS.www;
 
 console.log(strainsList);
 
@@ -10,9 +10,11 @@ const searchBtn = document.getElementById('searchBtn');
 const strainsContainer = document.getElementById('strainsContainer');
 
 const modal = document.getElementById('modal');
-const modalContent = document.getElementById('modalContent');
 const modalStrainInfo = document.getElementById('modalStrainInfo');
 const modalCloseBtn = document.querySelector('.close');
+
+// Display all strains initially
+displayAllStrains(strainsList);
 
 searchBtn.addEventListener('click', ev => {
 	ev.preventDefault();
@@ -32,35 +34,45 @@ function performSearch() {
 	displayStrains(filteredStrains);
 }
 
-function searchStrains(searchTerm) {
-	const filteredStrains = strainsList.filter(strain => {
-		const strainName = strain.strain.trim().toLowerCase();
-		const strainNo = strain.no.trim().toLowerCase();
-		return strainName.includes(searchTerm) || strainNo.includes(searchTerm);
-	});
-	return filteredStrains;
+function displayAllStrains(strains) {
+	strainsContainer.innerHTML = '';
+
+	for (const strain of strains) {
+		const strainElement = createStrainElement(strain);
+		strainsContainer.appendChild(strainElement);
+	}
 }
 
-console.log(searchStrains('bacillus'));
+function searchStrains(searchTerm) {
+	let filteredStrains = [];
+
+	for (const strain of strainsList) {
+		const strainName = `${strain.Gatunek} KKP ${strain.KKP}`.toLowerCase();
+
+		if (strainName.includes(searchTerm)) {
+			filteredStrains.push(strain);
+		}
+	}
+
+	return filteredStrains;
+}
 
 function displayStrains(strains) {
 	strainsContainer.innerHTML = '';
 
-	strains.forEach(strain => {
-		const strainEl = document.createElement('div');
-		strainEl.classList.add('strain');
+	for (const strain of strains) {
+		const strainElement = createStrainElement(strain);
+		strainsContainer.appendChild(strainElement);
+	}
+}
 
-		const nameAndNumber = document.createElement('p');
-		nameAndNumber.textContent = `${strain.strain} ${strain.no}`;
-		nameAndNumber.classList.add('strain-info');
-		strainEl.appendChild(nameAndNumber);
-
-		strainEl.addEventListener('click', () => {
-			openModal(strain);
-		});
-
-		strainsContainer.appendChild(strainEl);
-	});
+function createStrainElement(strain) {
+	const strainElement = document.createElement('div');
+	strainElement.classList.add('strain-item');
+	const strainName = `${strain.Gatunek} KKP ${strain.KKP}`;
+	strainElement.innerHTML = `<span class="strain-info">${strainName}</span>`;
+	strainElement.addEventListener('click', () => openModal(strain));
+	return strainElement;
 }
 
 function openModal(strain) {
@@ -68,24 +80,28 @@ function openModal(strain) {
 	modalStrainInfo.innerHTML = '';
 
 	const modalTitle = document.createElement('h2');
-	modalTitle.textContent = `${strain.strain} ${strain.no}`;
+	modalTitle.textContent = `${strain.Gatunek} KKP ${strain.KKP}`;
+	modalTitle.classList.add('modal-title');
 	modalStrainInfo.appendChild(modalTitle);
 
 	const modalInfoList = document.createElement('ul');
+	modalInfoList.classList.add('modal-info-list');
 
-	const isolationInfo = strain.isolation;
-	if (isolationInfo) {
-		const isolationItem = document.createElement('li');
-		isolationItem.textContent = `Isolation Year: ${isolationInfo.year}, Place: ${isolationInfo.place}`;
-		modalInfoList.appendChild(isolationItem);
-	}
-
-	const growthConditionsInfo = strain.growthConditions;
-	if (growthConditionsInfo) {
-		const growthConditionsItem = document.createElement('li');
-		growthConditionsItem.textContent = `Growth Temperature: ${growthConditionsInfo.temperature}, Conditions: ${growthConditionsInfo.conditions}`;
-		modalInfoList.appendChild(growthConditionsItem);
-	}
+	modalInfoList.innerHTML = `
+		<p class="section-header">Warunki hodowli:</p>
+		<li class="modal-info-item">Pożywka: ${strain.Pozywki || 'brak danych'}</li>
+		<li class="modal-info-item">Temperatura: ${strain.Temperatura || 'brak danych'} °C</li>
+		<li class="modal-info-item">Czas hodowli: ${strain.CzasHodowli || 'brak danych'}</li>
+		<li class="modal-info-item">Wymagania atmosferyczne: ${
+			strain.WymaganiaAtmosferyczne || 'brak danych'
+		}</li>
+		<p class="section-header">Informacje o szczepie:</p>
+		<li class="modal-info-item">Rok izolacji: ${strain.RokIzolacji || 'brak danych'}</li>
+		<li class="modal-info-item">Kraj pochodzenia: ${strain.KrajPochodzenia || 'brak danych'}</li>
+		<li class="modal-info-item">GenBank: <a class="modal-link" href="https://www.ncbi.nlm.nih.gov/nuccore/${
+			strain.Genbank
+		}" target="_blank">${strain.Genbank}</a></li>
+	`;
 
 	modalStrainInfo.appendChild(modalInfoList);
 
