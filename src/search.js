a8539394f974ab91966e1c4c87e0ecafcbe90c1e
Adrian Wojtczak <117528537+adrianwojtczak@users.noreply.github.com>
2024-02-19 00:03:55 +0100

    temp fix

--- src/search.js
+++ src/search.js
@@ -152,7 +152,7 @@ function openModal(strain) {
 				.filter(medium => medium) // Filtruje wartości, eliminuje undefined
 				.map(medium =>
 					medium
-						? `<a class="modal-link" href="/src/docs/medium/${medium}.pdf" target="_blank">${medium}</a>`
+						? `<a class="modal-link" href="/KKP-MicroStrainSearch/docs/medium/${medium}.pdf" target="_blank">${medium}</a>`
 						: ''
 				)
 				.join(', ') || 'brak danych'
