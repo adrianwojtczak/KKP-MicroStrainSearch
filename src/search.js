ce80288802a106f7ce34c9e59bafca8d79cc4ac5
Adrian Wojtczak <117528537+adrianwojtczak@users.noreply.github.com>
2024-02-18 23:53:46 +0100

    fix

--- src/search.js
+++ src/search.js
@@ -115,62 +115,6 @@ function createStrainElement(strain) {
 	return strainElement;
 }
 
-// function openModal(strain) {
-// 	modal.style.display = 'block';
-// 	modalStrainInfo.innerHTML = '';
-
-// 	const modalTitle = document.createElement('h2');
-// 	modalTitle.textContent = `${strain.Rodzaj} ${strain.Gatunek} KKP ${strain.KKP}`;
-// 	modalTitle.classList.add('modal-title');
-// 	modalStrainInfo.appendChild(modalTitle);
-
-// 	const modalInfoList = document.createElement('ul');
-// 	modalInfoList.classList.add('modal-info-list');
-
-// 	modalInfoList.innerHTML = `
-// 		<p class="section-header">Informacje o szczepie:</p>
-
-// 		<li class="modal-info-item">Autor nazwy: ${strain.AutorNazwy || 'brak danych'}</li>
-// 		<li class="modal-info-item">Data pobrania probki: ${strain.DataPobraniaProbki || 'brak danych'}</li>
-// 		<li class="modal-info-item">Inne oznakowanie: ${strain.Oznakowanie || 'brak danych'}</li>
-// 		<li class="modal-info-item">Deponujący: ${strain.Deponujacy || 'brak danych'}</li>
-// 		<li class="modal-info-item">Grupa patogenna: ${
-// 			strain.GrupaPatogenna
-// 				? `${strain.GrupaPatogenna} <b>(zgodnie z Rozporządzeniem Ministra Zdrowia z dnia 11 grudnia 2020 r.)</b>`
-// 				: 'brak danych'
-// 		}</li>
-// 		<li class="modal-info-item">Rok izolacji: ${strain.RokIzolacji || 'brak danych'}</li>
-// 		<li class="modal-info-item">Kraj pochodzenia: ${strain.Kraj || 'brak danych'}</li>
-// 		<li class="modal-info-item">GenBank: <a class="modal-link" href="https://www.ncbi.nlm.nih.gov/nuccore/${
-// 			strain.Genbank
-// 		}" target="_blank">${strain.Genbank}</a></li>
-// 		<p class="section-header">Warunki hodowli:</p>
-// 		<li class="modal-info-item">Pożywki: ${
-// 			[strain.Medium1, strain.Medium2, strain.Medium3]
-// 				.filter(medium => medium) // Filtruje wartości, eliminuje undefined
-// 				.map(medium =>
-// 					medium
-// 						? `<a class="modal-link" href="https://github.com/adrianwojtczak/KKP-MicroStrainSearch/blob/main/src/docs/medium/${medium}.pdf" target="_blank">${medium}</a>`
-// 						: ''
-// 				)
-// 				.join(', ') || 'brak danych'
-// 		}</li>
-// 		<li class="modal-info-item">Temperatura: ${
-// 			strain.Temperatura !== undefined ? `${strain.Temperatura} °C` : 'brak danych'
-// 		}</li>
-// 		<li class="modal-info-item">Wymagania atmosferyczne: ${
-// 			strain.WymaganiaAtmosferyczne || 'brak danych'
-// 		}</li>
-// 	`;
-
-// 	modalStrainInfo.appendChild(modalInfoList);
-
-// 	// Add Listeners
-// 	modalCloseBtn.addEventListener('click', closeModal);
-// 	window.addEventListener('click', outsideClick);
-// 	document.addEventListener('keydown', onEscKeyDown);
-// }
-
 function openModal(strain) {
 	modal.style.display = 'block';
 	modalStrainInfo.innerHTML = '';
