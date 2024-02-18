f5cc6610c7bf147036dd612a8d21ecc116182181
adrianwojtczak <117528537+adrianwojtczak@users.noreply.github.com>
2024-02-18 23:04:37 +0000

    Deploying to gh-pages from @ adrianwojtczak/KKP-MicroStrainSearch@a8539394f974ab91966e1c4c87e0ecafcbe90c1e 🚀

--- search.164d8f2e.js
+++ search.164d8f2e.js
@@ -11,13 +11,13 @@
 	  <li class="modal-info-item">GenBank: <a class="modal-link" href="https://www.ncbi.nlm.nih.gov/nuccore/${a.Genbank}" target="_blank">${a.Genbank}</a></li>
 	  <p class="section-header">Warunki hodowli:</p>
 	  <li class="modal-info-item">Pożywki: ${[a.Medium1,a.Medium2,a.Medium3].filter(a=>a)// Filtruje wartości, eliminuje undefined
-    .map(a=>a?`<a class="modal-link" href="/src/docs/medium/${a}.pdf" target="_blank">${a}</a>`:"").join(", ")||"brak danych"}</li>
+    .map(a=>a?`<a class="modal-link" href="/KKP-MicroStrainSearch/docs/medium/${a}.pdf" target="_blank">${a}</a>`:"").join(", ")||"brak danych"}</li>
 	  <li class="modal-info-item">Temperatura: ${void 0!==a.Temperatura?`${a.Temperatura} \xb0C`:"brak danych"}</li>
 	  <li class="modal-info-item">Wymagania atmosferyczne: ${a.WymaganiaAtmosferyczne||"brak danych"}</li>
 	`,u.appendChild(o),// Add Listeners
     c.addEventListener("click",K),window.addEventListener("click",d),document.addEventListener("keydown",y)})(a)),e}function K(){t.style.display="none",// Remove Listeners
 c.removeEventListener("click",K),window.removeEventListener("click",d),document.removeEventListener("keydown",y)}function d(a){a.target===t&&K()}function y(a){"Escape"===a.key&&K()}// Display all strains initially
 (function(a){for(let e of(n.innerHTML="",a)){let a=P(e);n.appendChild(a)}})(e),i.addEventListener("click",a=>{a.preventDefault(),z()}),r.addEventListener("submit",a=>{a.preventDefault(),z()}),// Add event listeners to checkboxes
-l.addEventListener("change",z),k.addEventListener("change",z),p.addEventListener("change",z),s.addEventListener("change",z)}();//# sourceMappingURL=search.12332da9.js.map
+l.addEventListener("change",z),k.addEventListener("change",z),p.addEventListener("change",z),s.addEventListener("change",z)}();//# sourceMappingURL=search.164d8f2e.js.map
 
-//# sourceMappingURL=search.12332da9.js.map
+//# sourceMappingURL=search.164d8f2e.js.map
