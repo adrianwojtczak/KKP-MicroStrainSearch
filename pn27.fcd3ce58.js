1ecc7882891a7e13ec54e35baf232c4b9172e3f7
adrianwojtczak <117528537+adrianwojtczak@users.noreply.github.com>
2024-05-22 14:16:04 +0000

    Deploying to gh-pages from @ adrianwojtczak/KKP-MicroStrainSearch@0afb5d3f372fb0b46970799d24d738c7325aa1f9 🚀

---
+++ pn27.fcd3ce58.js
@@ -0,0 +1,6 @@
+// Data docelowa
+const e=new Date("2024-05-23T14:30:59").getTime(),n=setInterval(function(){// Obecna data i czas
+let t=new Date().getTime(),a=e-t;// Wyświetlanie wyników w odpowiednich elementach HTML
+document.getElementById("days").innerText=Math.floor(a/864e5),document.getElementById("hours").innerText=Math.floor(a%864e5/36e5),document.getElementById("minutes").innerText=Math.floor(a%36e5/6e4),document.getElementById("seconds").innerText=Math.floor(a%6e4/1e3),a<0&&(clearInterval(n),document.getElementById("countdown").innerHTML='<span class="quote">Oszukiwanie może przynieść chwilowe korzyści, ale prawdziwe wartości i osiągnięcia wymagają cierpliwości i czasu. Pamiętaj, że na najważniejsze rzeczy w życiu warto poczekać.</span>')},1e3);//# sourceMappingURL=pn27.fcd3ce58.js.map
+
+//# sourceMappingURL=pn27.fcd3ce58.js.map
