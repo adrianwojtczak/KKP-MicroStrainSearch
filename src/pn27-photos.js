77125ff56b24e6bf7dee45c78a7737dbdb816ecc
Adrian Wojtczak <117528537+adrianwojtczak@users.noreply.github.com>
2024-06-27 15:01:27 +0200

    Update: photos

---
+++ src/pn27-photos.js
@@ -0,0 +1,33 @@
+import data from "./data/pn27.json";
+
+document.getElementById("search-button").addEventListener("click", function () {
+  const input = document.getElementById("search-input").value;
+  const result = data.find((item) => item.code === input);
+
+  const resultContainer = document.getElementById("result-container");
+  resultContainer.innerHTML = "";
+
+  if (result) {
+    const name = document.createElement("p");
+    name.textContent = result.name;
+
+    console.log(result);
+
+    const img = document.createElement("img");
+    img.src = result.URL;
+    img.alt = result.name;
+    img.className = "result-image";
+
+    const downloadButton = document.createElement("a");
+    downloadButton.href = result.URL;
+    downloadButton.download = result.name;
+    downloadButton.textContent = "Pobierz obraz";
+    downloadButton.className = "download-button";
+
+    resultContainer.appendChild(name);
+    resultContainer.appendChild(img);
+    resultContainer.appendChild(downloadButton);
+  } else {
+    resultContainer.textContent = "Nie znaleziono wyniku.";
+  }
+});
